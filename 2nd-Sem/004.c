e6e7bfa88b038387f2c3766893a527c421e10d6b
Swapnil Shelke <swapnilbabanshelke@gmail.com>
2022-05-25 20:04:38 +0530

    Kara fakt copy paste

--- 2nd-Sem/004.c
+++ 2nd-Sem/004.c
@@ -4,11 +4,11 @@
 // DITMS
 
 // String manipulation function e.g. 
-// string copy, 
-// concatenation, 
-// compare, 
-// string length, 
-// reverse
+// string copy, ✔
+// concatenation, ✔
+// compare, ✔
+// string length, ✔ 
+// reverse 
 
 #include<stdio.h> 
 #include<string.h> 
@@ -17,29 +17,41 @@ int main(){
 int num; 
 char str1[10]= "Swapnil", str2[10]= "Shelke";
  
-//  string contenation / joining two strings
-strcat(str1, str2); 
-puts(str1);
+//  string contenation 
+// strcat(str1, str2); 
+// puts(str1);
+
+// string uppercase
+strupr(str2);
+puts(str2);
 
 // string copy function 
-strcpy(str1, str2); 
-puts(str1);
+// strcpy(str1, str2); 
+// puts(str1);
 
 // string length 
-strlen(str2);
-printf("\nString length of str1 is : %d", str2);
+// strlen(str2);
+// printf("\nString length of str1 is : %d", str2);
 
 // string copy 
-if(strcmp(str1,str2)==0){
-   printf("\n String1 and string two are equal");
-}
-else{
-    printf("\n String1 an dstring2 are different");
-}
+// if(strcmp(str1,str2)==0){
+//    printf("\n String1 and string two are equal");
+// }
+// else{
+//     printf("\n String1 an dstring2 are different");
+// }
 
+
+// string reverse
+// strrev(str1);
+// puts(str1);
 
 return 0; 
 }
 
-
-// https://beginnersbook.com/2014/01/c-strings-string-functions/
+// Output 
+// SwapnilShelke
+// SHELKE
+// Shelke
+// 6 
+// String1 an dstring2 are different
--- 2nd-Sem/009.c
+++ 2nd-Sem/009.c
@@ -4,3 +4,133 @@
 // DITMS
 
 // Program to delete a record from binary file
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+struct person{
+    char *name;
+    int code;
+    int number;
+};
+
+int createBinFile(char *fname){
+    FILE *fp;
+    struct person newrecord; 
+
+    // Open for binary writing 
+    fp=fopen(fname, "wb");
+    if(!fp){
+          printf("Unable to open file!");
+          return -1;
+    }
+
+    newrecord.name="aaa bbbb"; 
+    newrecord.code=12345; 
+    newrecord.number=1; 
+    fwrite(&newrecord, sizeof(struct person), 1, fp); 
+
+    
+    newrecord.name="aaa bbbb"; 
+    newrecord.code=34567; 
+    newrecord.number=2; 
+    fwrite(&newrecord, sizeof(struct person), 1, fp); 
+
+    newrecord.name="aaa bbbb"; 
+    newrecord.code=12345; 
+    newrecord.number=3; 
+    fwrite(&newrecord, sizeof(struct person), 1, fp); 
+
+   fclose(fp);
+   return 0; 
+}
+
+int readBinFile(char *fname){
+    FILE *fp; 
+    struct person myrecord; 
+
+    fp=open(fname, "rb");
+    if(!fp){
+       printf("Unable to open file!");
+       return -1;
+    }
+
+    printf("The following records are in the binary fiel %s:\n", fname); 
+    while(fread(&myrecord, sizeof(struct person),1,fp)!=NULL){
+       printf("%s\n",myrecord.name);
+       printf("%d\n",myrecord.code);
+       printf("%d\n",myrecord.number);
+    }
+    fclose(fp);
+    return 0; 
+}
+
+int deleteRecordByName(char *fname, char *searchname){
+    FILE *fp;
+    FILE *fp_tmp;
+    int found=0; 
+    struct person myrecord; 
+
+    fp=fopen(fname,"rb");
+    if(!fp){
+        printf("Unable to open file %s", fname);
+        return -1;
+    }
+
+    fp_tmp=fopen("tmp.bin", "wb");
+    if(!fp_tmp){
+        printf("Unable to open the file");
+        return -1;
+    }
+
+    while (fread(&myrecord,sizeof(struct person),1,fp)!=NULL){
+      if(strcmp(searchname, myrecord.name)==0){
+          printf("A record with request name found and deleted. \n\n");
+      }
+      else{
+          fwrite(&myrecord, sizeof(struct person), 1, fp_tmp);
+      }
+    }
+
+    if(! found){
+         printf("No records found with the requested name: %s\n\n," ,searchname);
+    }
+    fclose(fp);
+    fclose(fp_tmp);
+
+    remove(fname);
+    rename("tmp.bin",fname);
+
+    return 0;
+}
+
+int main(){
+    int result, errno;
+
+    // setup a new file  on each run. 
+    result = createBinFile("test.bin");
+    if(result == -1){
+        perror("Erro rcreating bin file!");
+        exit(1);
+    }
+
+    result= readBinFile("test.bin");
+    if(result == -1){
+        perror("Erro reading bin file!");
+        exit(1);
+    }
+
+    result = deleteRecordByName("test.bin","cc ddd");
+    if(result == -1){
+       perror("Error deleting record!"); 
+       exit(1);
+    }
+
+
+    result = readBinFile("test.bin");
+    if(result == -1){
+       perror("Error reading bin file!");
+       exit(1);
+    }
+    return 0;
+}
